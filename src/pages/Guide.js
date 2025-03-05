92e80a5145c2b6300b418bae446e4212a0f478e2
gohier-mathieu <mathieugohier.pro@gmail.com>
2025-03-05 16:22:28 +0100

    ajout badges

--- src/pages/Guide.js
+++ src/pages/Guide.js
@@ -10,6 +10,7 @@ function Guide() {
     { id: 2, title: 'Nombre de piéces de drones dont vous avez besoin pour chaque palier de drone', imageUrl: 'img/divers/drone.png' },
     { id: 3, title: 'Ressources nécessaires pour mettre des étoiles sur vos équipements lvl40 (à faire si votre team est full équipements oranges) ', imageUrl: 'img/divers/etoiles.png' },
     { id: 3, title: "Nombre de pierres d'amélioration par level d'équipement\nmontez par palier de 10\ncouts par palier:\n,de 0 a 10 >20 800 pierres\n,de 11 a 20 > 38 800 pierres\n,de 21 a 30 > 58 200 pierres\n,de 31 a 40 > 76 200 pierres  ", imageUrl: 'img/divers/pierre.png' },
+    { id: 3, title: 'Badges de competences', imageUrl: 'img/divers/badges.jpg' },
   ];
 
   return (
